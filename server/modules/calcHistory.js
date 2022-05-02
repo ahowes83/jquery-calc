d511d8ad23a3778a74fa2d545c7ac4b7a5a8f306
ahowes83 <a_howes83@yahoo.com>
2022-05-02 01:57:21 -0500

    fixed server in/out, not sure where to put/run calculating function

--- server/modules/calcHistory.js
+++ server/modules/calcHistory.js
@@ -2,29 +2,23 @@ const express = require('express');
 const router = express.Router();
 let history = [];
 
-router.get( '/history', (req, res)=>{
-  console.log( '/calc GET' );
-  calculate();
-  res.send( history );
-})
-
-function piCheck(){
-  if (history[i].operandOne === '3.14159') {
-    history[i].operandOne = Math.PI;
+function piCheck(histList){
+  if (histList[i].operandOne === '3.14159') {
+    histList[i].operandOne = Math.PI;
   }
-  if (history[i].operandTwo === '3.14159'){
-    history[i].operandTwo = Math.PI;
+  if (histList[i].operandTwo === '3.14159'){
+    histList[i].operandTwo = Math.PI;
   }
 }
 
-function calculate(){
-  piCheck();
+function calculate(histList){
+  piCheck(histList);
 
-  for(let i=0; i< history.length; i++){
-    let operator = history[i].operator;
-    let result = history[i].result;
-    let opOne = Number(history[i].operandOne);
-    let opTwo = Number(history[i].operandTwo);
+  for(let i=0; i< histList.length; i++){
+    let operator = histList[i].operator;
+    let result = histList[i].result;
+    let opOne = Number(histList[i].operandOne);
+    let opTwo = Number(histList[i].operandTwo);
 
     switch (operator) {
       case '*':
@@ -48,7 +42,14 @@ function calculate(){
   }
 }
 
-router.post('/history', (req, res)=>{
+router.get( '/', (req, res)=>{
+  console.log( '/calc GET' );
+  res.send( history );
+})
+
+
+
+router.post('/', (req, res)=>{
   console.log('/calc POST:', req.body);
   history.push(req.body);
   res.sendStatus(200);
--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
+//import * as logic from '/modules/logic.js');
+
 const express = require('express');
 const app = express();
 const router = express.Router();
 const bodyParser = require('body-parser');
 const history = require('./modules/calcHistory');
-const logic = require('./modules/logic');
+//const logic = require('./modules/logic');
 
 app.use(express.static('./server/public'));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use( '/calcHistory', history);
-app.get('/calc', function (req, res) {
-  res.send(logic.calculate(history));
-});
+//app.get('/calc', function (req, res) {
+ // res.send(logic.calculate());
+//});
 
 const port = 5001;
 
